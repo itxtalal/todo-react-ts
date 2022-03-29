0ef38e7d72725c7816e511ed9c7fc8914b5f2325
Talal <talalstudent@gmail.com>
2022-03-29 13:55:08 +0500

    State in Typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
+import { useState } from "react";
 import "./App.css";
+
 import NewTodo from "./components/NewTodo";
 import Todos from "./components/Todos";
 import Todo from "./models/Todo";
 
 function App() {
-  const todos = [new Todo("Learn React"), new Todo("Learn Typescript")];
+  const [todos, setTodos] = useState<Todo[]>([]);
 
   const addToDoHandler = (todoText: string) => {
     console.log(todoText);
+    const newTodo = new Todo(todoText);
+
+    setTodos((currentState) => {
+      return currentState.concat(newTodo);
+    });
   };
 
   return (
